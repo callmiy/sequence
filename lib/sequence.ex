41efe57044d7e50fa063834b25a17d97daeef598
maneptha <maneptha@gmail.com>
2018-12-27 07:23:31 +0100

    initial commit

---
+++ lib/sequence.ex
@@ -0,0 +1,88 @@
+defmodule Sequence do
+  use Agent
+
+  @moduledoc """
+  Module for generating sequential values.
+
+  Use `sequence/1` or `sequence/2` to generate
+  sequential values instead of calling this module directly.
+  """
+
+  @doc false
+  def start_link(_) do
+    Agent.start_link(fn -> Map.new() end, name: __MODULE__)
+  end
+
+  @doc """
+  Reset all sequences so that the next sequence starts from 0
+
+  ## Example
+
+      Sequence.next("joe") # "joe0"
+      Sequence.next("joe") # "joe1"
+
+      Sequence.reset
+
+      Sequence.next("joe") # resets so the return value is "joe0"
+
+  You can use list as well
+
+      Sequence.next(["A", "B"]) # "A"
+      Sequence.next(["A", "B"]) # "B"
+
+      Sequence.reset
+
+      Sequence.next(["A", "B"]) # resets so the return value is "A"
+
+  Using formatter
+      # Will generate "me-0@foo.com" then "me-1@foo.com", etc.
+      Sequence.next(:email, &"me-\#{&1}@foo.com")
+
+      # Will generate "admin" then "user", "other", "admin" etc.
+      # `role` is the name of this sequence
+      Sequence,next(:role, ["admin", "user", "other"])
+
+  If you want to reset sequences at the beginning of every test, put it in a
+  `setup` block in your test.
+
+      setup do
+        Sequence.reset
+      end
+  """
+
+  @spec reset() :: :ok
+  def reset do
+    Agent.update(__MODULE__, fn _ -> Map.new() end)
+  end
+
+  def next(sequence_name) when is_binary(sequence_name) do
+    next(sequence_name, &(sequence_name <> to_string(&1)))
+  end
+
+  def next(sequence_name) do
+    raise(
+      ArgumentError,
+      "Sequence name must be a string, got #{inspect(sequence_name)} instead"
+    )
+  end
+
+  def next(sequence_name, [_ | _] = list) do
+    length = length(list)
+
+    Agent.get_and_update(__MODULE__, fn sequences ->
+      current_value = Map.get(sequences, sequence_name, 0)
+      index = rem(current_value, length)
+      new_sequences = Map.put(sequences, sequence_name, index + 1)
+      {value, _} = List.pop_at(list, index)
+      {value, new_sequences}
+    end)
+  end
+
+  def next(sequence_name, formatter) do
+    Agent.get_and_update(__MODULE__, fn sequences ->
+      current_value = Map.get(sequences, sequence_name, 0)
+      new_sequences = Map.put(sequences, sequence_name, current_value + 1)
+      {formatter.(current_value), new_sequences}
+    end)
+  end
+end
